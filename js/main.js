b408d7db79f30edbd57f53d4d646788bebadd009
Bruno Carbonneau <accesshockeystats@gmail.com>
2020-11-20 21:33:38 -0500

    Creating the fuctions for the choice of the player and the random choice of the computer.

--- js/main.js
+++ js/main.js
@@ -7,8 +7,66 @@
 //Si gagnant parties terminées, on peut reocmmance une partie ou non selon la réponse du joueur
 //Sinon on continue une autre manche 
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+function playRound(){
+  const player = playerChoice();
+  const computer = computerPlay();
+
+  console.log(player);
+  console.log(computer);
+
+  if(player === 'paper' && computer === 'rock'){
+    return 'player';
+  }else if(player === 'paper' && computer === 'scissors'){
+    return 'computer';
+  }else if(player === 'rock' && computer === 'scissors'){
+    return 'rock';
+  }else if(player === 'rock' && computer === 'paper'){
+    return 'computer';
+  }else if(player === 'scissors' && computer === 'rock'){
+    return 'computer';
+  }else if(player === 'scissors' && computer === 'paper'){
+    return 'player';
+  }else{
+    return 'draw';
+  }
+    
+
+}
+
+function playerChoice(){
+   const the_player_choice = prompt("Choose: Paper, Rock or Scissors")
+   return the_player_choice.toLowerCase();
+}
+
+
+function computerPlay(){
+  const resultRandom = getRandomInt(3);
+  let computerChoice
+
+  switch (resultRandom){
+    case 0: computerChoice = 'rock';
+      break;
+    case 1: computerChoice = 'paper';
+      break;
+    case 2: computerChoice = 'scissors';
+      break;
+    default: computerChoice = 'Unknown choice';
+      break;
   }
 
-console.log(getRandomInt(3));
+  return computerChoice;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+let startGame = 'play';
+//while(startGame === 'play'){
+  let gameOver = false;
+//  while(!gameOver){
+  const resultRound = playRound();
+//  }
+   console.log(resultRound); 
+//}
+  
